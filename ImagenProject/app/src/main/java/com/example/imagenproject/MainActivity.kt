a86c157e1d3e57d63853acddd607c9a032e0eb50
Aitor <aitor@max12>
2025-11-18 13:02:15 +0100

    Nuevos Jetpack

--- ImagenProject/app/src/main/java/com/example/imagenproject/MainActivity.kt
+++ ImagenProject/app/src/main/java/com/example/imagenproject/MainActivity.kt
@@ -1,27 +1,38 @@
 package com.example.imagenproject
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.example.imagenproject.R.drawable.logo
@@ -39,29 +50,49 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun Imagen() {
+    // Declaramos una variable observable para almacenar el color de fondo
+    // val colorFondo = remember { mutableStateOf(Color.White) }
+    var colorFondo by remember { mutableStateOf(Color.White) }
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+            .background(colorFondo)
     ) {
         Image(
             painter = painterResource(id = R.drawable.logo),
-            contentDescription = "Imagen de mi futura empresa"
+            contentDescription = "Imagen de mi futura empresa",
+            modifier = Modifier
+                .align(Alignment.Center)
+                .fillMaxSize()
         )
         Text(
             text = "El Logo de mi Futura Empresa",
-            color = colorResource(id = R.color.teal_200),
-            fontSize = 20.sp,
-            fontFamily = FontFamily.SansSerif,
+            color = Color.Gray,
+            fontSize = 23.sp,
             fontWeight = FontWeight.Bold,
             fontStyle = FontStyle.Italic,
-            letterSpacing = 1.sp,
-            textDecoration = TextDecoration.None,
-            textAlign = TextAlign.Right,
-            lineHeight = 2.em,
-            maxLines = 4
+            textAlign = TextAlign.Center,
+            modifier = Modifier.align(Alignment.Center)
         )
+        Button(
+            onClick = {colorFondo = colorAleatorio()},
+            modifier = Modifier.align(Alignment.BottomEnd)
+
+        ) {
+            Text(text = "Cambiar fondo")
+        }
     }
+}
+
+fun colorAleatorio(): Color {
+    val rojo = kotlin.random.Random.nextFloat()
+    // val rojoN = (0..255).random().toFloat()
+    val verde = kotlin.random.Random.nextFloat()
+    val azul = kotlin.random.Random.nextFloat()
+
+    return Color(rojo, verde, azul)
 }
